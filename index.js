1dac8ef0f31abda1bd5816e2665200a341174dac
unknown <deepchopda01@gmail.com>
2025-01-10 17:55:38 +0530

    10-01-25

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 require('./config.js')
-const user = require('./model/user.model.js')
+const users = require('./model/user.model.js')
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,9 +12,10 @@ const secrate_key = "secratekey"
 
 function verifytoken(req, res, next) {
     const bearerHeader = req.headers['authorization']
-    console.log(bearerHeader)   
+    console.log(bearerHeader)
     if (typeof bearerHeader !== 'undefined') {
-        const bearer =  bearerHeader.split(" ");
+        const bearer = bearerHeader.split(" ");
+        console.log(bearer)
         const token = bearer[1];
         req.token = token
         next();
@@ -25,13 +26,13 @@ function verifytoken(req, res, next) {
     }
 }
 
-app.post('/profile',verifytoken,(req,res)=>{
-    jwt.verify(req.token,secrate_key,(err,authData)=>{
-        if(err){
-            res.send({result:"token is invalid"})
-        }else{
+app.post('/profile', verifytoken ,(req, res) => {
+    jwt.verify(req.token, secrate_key, (err, authData) => {
+        if (err) {
+            res.send({ result: "token is invalid" })
+        } else {
             res.json({
-                message:'profile accesed',
+                message: 'profile accesed',
                 authData
             })
         }
@@ -39,27 +40,32 @@ app.post('/profile',verifytoken,(req,res)=>{
 })
 
 app.post('/login', async (req, res) => {
-    const user = {
-        id: 1,
-        name: "ram",
-        email: "csfgrg@vfg.ged"
-    }
-    jwt.sign({ user }, secrate_key, { expiresIn: '1200s' }, (err, token) => {
-        res.json({ token })
+    users.findOne({ email: req.body.email }).then((user) => {
+        const id=user._id
+        jwt.sign({id}, secrate_key, { expiresIn: '1200s' }, (err, token) => {
+            res.json({ token })
+        })
+    }).catch(()=>{
+        res.send("user not found");
     })
 })
-
-const verification = () => {
-
-}
 
 app.get("/get", async (req, res) => {
     res.send("data getted");
 })
 app.post("/insert", async (req, res) => {
-    let data = await user.create(req.body);
-    res.send(data);
-    console.log(data);
+    users.findOne({ email: req.body.email }).then((user) => {
+        if (user) {
+            return res.status(400).send(`An account with the email ${req.body.email} already exists`)
+        } else {
+            users.create(req.body).then((data) => {
+                res.status(201).send(data)
+            }).catch((err) => {
+                console.log(err);
+                res.status(500).send(`error ${err}`)
+            })
+        }
+    })
 })
 app.listen(port, () => {
     console.log(`app is running on ${port}`)
